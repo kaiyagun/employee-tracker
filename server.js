2c6f48fc1fd1c8204d642de84b559b933e23564d
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-10-25 21:59:26 -0700

    framework for application done, just need to figure out how to make the db info become options on the inquirer prompts

--- server.js
+++ server.js
@@ -31,24 +31,28 @@ inquirer
       ],
     },
   ]).then((response) => {
+
       if(response.choices === 0) {
+          //show employees
           db.query('SELECT employee.first_name, employee.last_name, department.id, department.dept_name, roles.title, roles.salary FROM department JOIN roles ON department.id = roles.department_id JOIN employee ON roles.id = employee.role_id;', function (err, results) {
               console.table(results);
           });
 
-      } else if(response.choices === 1) {
-          const roleChoices = [];
-          const chooseRole = () => db.query(`SELECT title FROM roles`, (err, result) => {
-            for (let i = 0; i < result.length; i++) {
-                const roles = result[i];
-
-                roleChoices.push(`{name: '` + roles[i] +`', value: '`+ i++ + `'}`)                
-            }
-          })
-          chooseRole()
-
-          
-          return inquirer.prompt([
+    } else if(response.choices === 2) {
+      //show roles
+          db.query('SELECT * FROM roles;', function (err, results) {
+              console.table(results);
+          }); 
+    
+    } else if(response.choices === 4 ) {
+        //show depts
+        db.query('SELECT * FROM department;', function(err, results) {
+            console.table(results);
+        })
+    } else if(response.choices === 1) {
+        // add employee
+        
+        return inquirer.prompt([
               {
                   type: 'input',
                   name: 'firstname',
@@ -62,12 +66,68 @@ inquirer
               {
                   type: 'list',
                   name: 'roles',
-                  choices: roleChoices
+                  choices: [{
+
+                  }]
               }
-          ])
-        //   db.query('INSERT INTO')
+          ]);
+    } else if(response.choices === 3) {
+        // add role
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'New role name?'
+            },
+            {
+                type: 'number',
+                name: 'salary',
+                message: 'Salary of new role?'
+            },
+            {
+                type: 'list',
+                name: 'newRoleDept',
+                choices: [{
+
+                }]
+            }
+        ])
+    } else if(response.choices === 5) {
+        //add dept
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newDept',
+                message:'New department name?'
+            }
+        ]).then(db.query(`INSERT INTO department (dept_name) VALUES (${response});`, function(err, results) {
+            console.log('Successfully added new department!')
+        })
+ );
+  } else if(response.choices === 6) {
+      // update role
+      return inquirer.prompt([
+          {
+              type: 'list',
+              name: 'employeeSelect',
+              message: 'Which employee would you like to update?'
+          },
+          {
+              type: 'list',
+              name: 'newEmployeeRole',
+              message: 'New role for employee?'
+          }
+      ])
+  }
+});
+        
+
+          
+          
+        //   const addEmployee = function() {db.query('INSERT INTO')
         //   console.log('Added employee')
-        }
-    });
+        // };
+        // addEmployee()
+  
   
   
