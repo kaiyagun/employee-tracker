c926280ad49834a198aa3a7437db0346e7b1e7b9
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-11-21 18:07:30 -0800

    added a few updates

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cTable = require("console.table");
 
 const mysql = require("mysql2");
 
+
+
 const db = mysql.createConnection(
 {
   host: "localhost",
@@ -13,6 +15,79 @@ const db = mysql.createConnection(
  console.log('Connected to employees_db database')
 );
 
+
+const rolesArr = [];
+const departmentsArr = [];
+const employeeFirstArr = [];
+const employeeLastArr=[];
+
+const showEmployees = function() {
+    db.query('SELECT employee.first_name, employee.last_name, department.id, department.dept_name, roles.title, roles.salary FROM department JOIN roles ON department.id = roles.department_id JOIN employee ON roles.id = employee.role_id;', function (err, results) {
+        console.table(results);
+});
+};
+
+const restart = () => {
+    // promt if they want to continue
+    inquirer.prompt({
+        type: 'list',
+        message: 'Go back to main menu?',
+        name: 'choice',
+        choices: ['yes','no']
+    }).then( ans => {
+        if(ans.choice == 'yes'){
+            chooseAction();
+        }
+    });
+    
+}
+  
+const showRoles = function() {
+    db.query('SELECT * FROM roles;', function (err, results) {
+              console.table(results);
+          });
+
+    restart();
+}
+
+const showDepartments = function() {
+     db.query('SELECT * FROM department;', function(err, results) {
+            console.table(results);
+        })
+        restart();
+};
+
+const addEmployee = function() {
+    const inquirerRoles = rolesArr.map(role => {
+    return {
+        name: role.name,
+        value: role
+    }
+})
+    inquirer.prompt([
+              {
+                  type: 'input',
+                  name: 'firstname',
+                  message: 'Employee first name?'
+              },
+              {
+                  type: 'input',
+                  name: 'lastname',
+                  message: 'Employee last name?'
+              },
+              {
+                  type: 'list',
+                  name: 'roles',
+                  choices: inquirerRoles
+              }
+          ]).then(({ firstname, lastname, roles }) => {
+              db.query("INSERT INTO employee (firstname, lastname, roles) VALUES (?, ?, ?);", [ans.firstname,ans.lastnameans.roles])
+              
+          });
+}
+
+
+function chooseAction() {
 
 inquirer
   .prompt([
@@ -33,44 +108,21 @@ inquirer
   ]).then((response) => {
 
       if(response.choices === 0) {
-          //show employees
-          db.query('SELECT employee.first_name, employee.last_name, department.id, department.dept_name, roles.title, roles.salary FROM department JOIN roles ON department.id = roles.department_id JOIN employee ON roles.id = employee.role_id;', function (err, results) {
-              console.table(results);
-          });
+          showEmployees();
 
     } else if(response.choices === 2) {
       //show roles
-          db.query('SELECT * FROM roles;', function (err, results) {
-              console.table(results);
-          }); 
+      showRoles();
     
     } else if(response.choices === 4 ) {
         //show depts
-        db.query('SELECT * FROM department;', function(err, results) {
-            console.table(results);
-        })
+        showDepartments();
+       
     } else if(response.choices === 1) {
         // add employee
+        addEmployee();
         
-        return inquirer.prompt([
-              {
-                  type: 'input',
-                  name: 'firstname',
-                  message: 'Employee first name?'
-              },
-              {
-                  type: 'input',
-                  name: 'lastname',
-                  message: 'Employee last name?'
-              },
-              {
-                  type: 'list',
-                  name: 'roles',
-                  choices: [{
-
-                  }]
-              }
-          ]);
+        
     } else if(response.choices === 3) {
         // add role
         return inquirer.prompt([
@@ -120,14 +172,10 @@ inquirer
       ])
   }
 });
+}
         
+chooseAction();
 
-          
-          
-        //   const addEmployee = function() {db.query('INSERT INTO')
-        //   console.log('Added employee')
-        // };
-        // addEmployee()
-  
+
   
   
--- routes/api/departments.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const db = require('../../config/connection');
-
-router.get('/', (req,res)=>{
-    const query
-})
